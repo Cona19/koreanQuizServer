aab22793070721bfd5e7b701438de68e7b92c717
Hyeonseok Oh <hyeonso19@gmail.com>
2016-07-22 17:11:38 +0000

    Add function which provide statistics

--- routes/index.js
+++ routes/index.js
@@ -48,6 +48,29 @@ module.exports = function(app, dbs)
 
     res.status(200).send();
   });
+
+  // GET ALL WORDS 
+  app.get('/api/record/:facebookUserId([0-9]+)', function(req,res){
+    console.log('get records');
+    var facebookUserId = Number(req.params.facebookUserId);
+    var statistics = {};
+    dbs.UserRecord.find({facebookUserId: facebookUserId}, {cntCorrect: 1, cntWrong: 1}, function(err, userRecords){
+      if(err) return res.status(500).send({error: 'database failure'});
+      statistics.cntCorrectTry = userRecords[0].cntCorrect;
+      statistics.cntWrongTry = userRecords[0].cntWrong;
+      dbs.UserWord.find({facebookUserId: facebookUserId, succeed: true}, {}, function(err, userWords){
+        if(err) return res.status(500).send({error: 'database failure'});
+        statistics.cntCorrectProblem = userWords.length;
+        dbs.UserWord.find({facebookUserId: facebookUserId, succeed: false}, {}, function(err, userWords){
+          if(err) return res.status(500).send({error: 'database failure'});
+          statistics.cntWrongProblem = userWords.length;
+          console.log(statistics);
+          res.json(statistics);
+        });
+      });
+    });
+  });
+
   // GET ALL WORDS 
   app.get('/api/words', function(req,res){
     console.log('get all words');
@@ -58,10 +81,10 @@ module.exports = function(app, dbs)
   });
 
   // GET SINGLE WORD 
-  app.get('/api/words/:word_id([0-9]+)', function(req, res){
+  app.get('/api/words/:wordId([0-9]+)', function(req, res){
     console.log('get single word');
-    var word_id = Number(req.params.word_id);
-    dbs.KoreanWord.find({id: word_id}, {id: 1, word: 1, explanation: 1},  function(err, koreanWords){
+    var wordId = Number(req.params.wordId);
+    dbs.KoreanWord.find({id: wordId}, {id: 1, word: 1, explanation: 1},  function(err, koreanWords){
       if(err) return res.status(500).json({error: err});
       if(koreanWords.length === 0) return res.status(404).json({error: 'word not found'});
       res.json(koreanWords[0]);
